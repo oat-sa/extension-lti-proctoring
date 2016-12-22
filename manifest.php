c0f372e7dcf8042f135cf0b7afa27b4ae4b3cc96
Joel Bout <joel@taotesting.com>
2016-12-22 18:14:36 +0100

    Added launch URL and monitoring screen

---
+++ manifest.php
@@ -0,0 +1,63 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
+ *               
+ * 
+ */
+use oat\ltiProctoring\controller\ProctoringTool;
+use oat\ltiProctoring\controller\Monitor;
+use oat\taoLti\models\classes\LtiRoles;
+use oat\tao\model\user\TaoRoles;
+
+/**
+ * Generated using taoDevTools 2.17.0
+ */
+return array(
+    'name' => 'ltiProctoring',
+    'label' => 'LTI Proctoring',
+    'description' => 'Grants access to the proctoring functionalities using LTI',
+    'license' => 'GPL-2.0',
+    'version' => '0.1.0',
+    'author' => 'Open Assessment Technologies SA',
+    'requires' => array(
+        'taoProctoring' => '>=4.0.0',
+        'ltiDeliveryProvider' => '>=1.6.0',
+    ),
+    'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#ltiProctoringManager',
+    'acl' => array(
+        array('grant', 'http://www.tao.lu/Ontologies/generis.rdf#ltiProctoringManager', array('ext'=>'ltiProctoring')),
+        array('grant', TaoRoles::ANONYMOUS, ProctoringTool::class),
+        array('grant', LtiRoles::CONTEXT_TEACHING_ASSISTANT, Monitor::class),
+    ),
+    'install' => array(
+    ),
+    'uninstall' => array(
+    ),
+    'routes' => array(
+        '/ltiProctoring' => 'oat\\ltiProctoring\\controller'
+    ),    
+    'constants' => array(
+        # views directory
+        "DIR_VIEWS" => __DIR__.DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
+        
+        #BASE URL (usually the domain root)
+        'BASE_URL' => ROOT_URL.'ltiProctoring/',
+        
+        #BASE WWW required by JS
+        'BASE_WWW' => ROOT_URL.'ltiProctoring/views/'
+    )
+);
