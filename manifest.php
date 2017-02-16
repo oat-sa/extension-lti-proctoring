8d83529d4fdb569f4b9989cfb1683529e8de7de9
Joel Bout <joel@taotesting.com>
2017-02-16 10:40:14 +0100

    Allow proctoring of multiple deliveries

--- manifest.php
+++ manifest.php
@@ -33,10 +33,10 @@ return array(
     'label' => 'LTI Proctoring',
     'description' => 'Grants access to the proctoring functionalities using LTI',
     'license' => 'GPL-2.0',
-    'version' => '0.2.1',
+    'version' => '0.3.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
-        'taoProctoring' => '>=4.4.0',
+        'taoProctoring' => '>=4.4.4',
         'ltiDeliveryProvider' => '>=1.7.0',
     ),
     'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#ltiProctoringManager',
