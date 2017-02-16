caa3adcb56ad866fcc0e6c1ed52e5711b32cdfb8
Mikhail Kamarouski <Komarouski@1pt.com>
2017-02-16 19:06:41 +0300

    Dependency version adjusted

--- manifest.php
+++ manifest.php
@@ -37,7 +37,7 @@ return array(
     'version' => '0.4.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
-        'taoProctoring' => '>=4.4.0',
+        'taoProctoring' => '>=4.5.0',
         'ltiDeliveryProvider' => '>=2.2.0',
         'taoLti' => '>=1.8.0',
     ),
