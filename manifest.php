debeeb779699809b5d588f0dc434bff3215788cb
Aleh Hutnikau <hutnikau@1pt.com>
2017-03-31 16:05:33 +0300

    retrieve session

--- manifest.php
+++ manifest.php
@@ -36,7 +36,7 @@ return array(
     'label' => 'LTI Proctoring',
     'description' => 'Grants access to the proctoring functionalities using LTI',
     'license' => 'GPL-2.0',
-    'version' => '0.8.3',
+    'version' => '0.8.4',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'taoLti' => '>=1.11.0',
