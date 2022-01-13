a7813bf8c7814dae221c2560c0d32af36363f5bd
Yohan Laborda <yohan.laborda@taotesting.com>
2022-01-13 07:53:28 +0000

    feat: new proctor service provider

---
+++ model/ProctorServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2022 (original work) Open Assessment Technologies SA;
+ *
+ */
+
+declare(strict_types=1);
+
+namespace oat\ltiProctoring\model;
+
+use oat\generis\model\DependencyInjection\ContainerServiceProviderInterface;
+use oat\ltiProctoring\model\delivery\AutoStartProctorService;
+use oat\oatbox\log\LoggerService;
+use oat\taoQtiTest\models\TestSessionService;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ServicesConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+
+final class ProctorServiceProvider implements ContainerServiceProviderInterface
+{
+    public function __invoke(ContainerConfigurator $configurator): void
+    {
+        $services = $configurator->services();
+        $this->configAutoStartProctorService($services);
+    }
+
+    private function configAutoStartProctorService(ServicesConfigurator $services): void
+    {
+        $services
+            ->set(AutoStartProctorService::class, AutoStartProctorService::class)
+            ->public()
+            ->args(
+                [
+                    service(TestSessionService::SERVICE_ID),
+                    service(LoggerService::SERVICE_ID)
+                ]
+            );
+    }
+}
