1d8cf7756b255e936bf0199667b71b5ce21ebec9
Ivan Klimchuk <ivan@taotesting.com>
2022-11-25 00:58:58 +0300

    feature: script for finishing stale deliveries

---
+++ scripts/tools/FinishStaleDeliveryExecutions.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2022 (original work) Open Assessment Technologies SA;
+ *
+ * @author Ivan Klimchuk <ivan@taotesting.com>
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoProctoring\scripts\tools;
+
+use common_Exception;
+use common_exception_Error;
+use common_exception_NotFound;
+use common_session_SessionManager;
+use OAT\Library\Lti1p3Core\Message\Payload\Claim\AgsClaim;
+use oat\oatbox\reporting\ReportInterface;
+use oat\oatbox\service\exception\InvalidServiceManagerException;
+use Laminas\ServiceManager\ServiceLocatorAwareTrait;
+use oat\taoDelivery\model\execution\DeliveryExecution;
+use oat\taoDelivery\model\execution\StateServiceInterface;
+use oat\taoLti\models\classes\LtiLaunchData;
+use oat\taoLti\models\classes\LtiVariableMissingException;
+use oat\taoLti\models\classes\TaoLtiSession;
+use oat\taoLti\models\classes\user\Lti1p3User;
+use oat\taoProctoring\model\deliveryLog\DeliveryLog;
+use oat\taoProctoring\model\implementation\DeliveryExecutionStateService;
+use oat\taoProctoring\scripts\TerminatePausedAssessment;
+
+final class FinishStaleDeliveryExecutions extends TerminatePausedAssessment
+{
+    use ServiceLocatorAwareTrait;
+
+    protected function getTerminationReasons(): array
+    {
+        return ['category' => 'Technical', 'subCategory' => 'Cleanup'];
+    }
+
+    /**
+     * @param DeliveryExecution $deliveryExecution
+     * @throws InvalidServiceManagerException
+     * @throws LtiVariableMissingException
+     * @throws common_Exception
+     * @throws common_exception_Error
+     * @throws common_exception_NotFound
+     */
+    protected function terminateExecution(DeliveryExecution $deliveryExecution): void
+    {
+        if ($this->wetRun !== true) {
+            $this->addReport(ReportInterface::TYPE_INFO, "Delivery execution {$deliveryExecution->getIdentifier()} should be finished (terminated).");
+            return;
+        }
+
+        /** @var DeliveryLog $deliveryLog */
+        $deliveryLog = $this->getServiceLocator()->get(DeliveryLog::SERVICE_ID);
+
+        $launchParameters = current($deliveryLog->get($deliveryExecution->getIdentifier(), 'LTI_LAUNCH_PARAMETERS'));
+
+        // Patch AGS
+        $launchParameters['data'][LtiLaunchData::AGS_CLAIMS] = AgsClaim::denormalize($launchParameters['data'][LtiLaunchData::AGS_CLAIMS]);
+
+        $ltiLaunchData = new LtiLaunchData($launchParameters['data'], []);
+
+        $user = new Lti1p3User($ltiLaunchData);
+        $user->setRegistrationId($ltiLaunchData->getVariable(LtiLaunchData::TOOL_CONSUMER_INSTANCE_ID));
+
+        common_session_SessionManager::startSession(TaoLtiSession::fromVersion1p3($user));
+
+        /** @var DeliveryExecutionStateService $deliveryExecutionStateService */
+        $deliveryExecutionStateService = $this->getServiceLocator()->get(StateServiceInterface::SERVICE_ID);
+
+        $deliveryExecutionStateService->finish(
+            $deliveryExecution,
+            [
+                'reasons' => $this->getTerminationReasons(),
+                'comment' => __('The assessment was automatically finished (terminated) by the system due to inactivity.'),
+            ]
+        );
+
+        $this->addReport(ReportInterface::TYPE_WARNING, "Delivery execution {$deliveryExecution->getIdentifier()} has been finished (terminated).");
+    }
+}
